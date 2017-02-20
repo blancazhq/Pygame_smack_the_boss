6da3738e3e51c9ef17b669a598ec95276eeaee53
Huiqi Zhou <blancazhou2011@gmail.com>
2017-02-19 19:28:36 -0500

    add timer, modified collision detecter

--- pygame_smack_the_boss.py
+++ pygame_smack_the_boss.py
@@ -4,36 +4,31 @@ import random
 play_again = True
 level_counter = 1
 
+
 def overlap(c1, c2):
-    if (c2.x in xrange(c1.x,c1.x + c1.dimensions[2]) and c2.y in xrange(c1.y, c1.y + c1.dimensions[3])) or (c2.x + c2.dimensions[2] in xrange(c1.x,c1.x + c1.dimensions[2]) and c2.y + c2.dimensions[3] in xrange(c1.y, c1.y + c1.dimensions[3])):
+    if (c2.x in xrange(c1.x,c1.x + c1.dimensions[2]) and c2.y in xrange(c1.y, c1.y + c1.dimensions[3])) or (c2.x + c2.dimensions[2] in xrange(c1.x,c1.x + c1.dimensions[2]) and c2.y + c2.dimensions[3] in xrange(c1.y, c1.y + c1.dimensions[3])) or (c1.x in xrange(c2.x,c2.x + c1.dimensions[2]) and c1.y in xrange(c2.y, c2.y + c1.dimensions[3])) or (c1.x + c1.dimensions[2] in xrange(c2.x,c2.x + c2.dimensions[2]) and c1.y + c1.dimensions[3] in xrange(c2.y, c2.y + c2.dimensions[3])):
         return True
     else:
         return False
 
 class Boss(object):
-    def __init__(self,image,screen,others):
+    def __init__(self,image,screen,others,tools):
         self.image = image
         self.dimensions = image.get_rect()
         self.bounds = screen.get_rect()
-        self.set_location(others)
+        self.set_location(others,tools)
         self.direction_speed = 1
-        self.change_direction()
 
-    def change_direction(self):
-        self.x_speed = random.randint(-1,1)
-        self.y_speed = random.randint(-1,1)
-        self.x_speed *= self.direction_speed
-        self.y_speed *= self.direction_speed
-
-    def set_location(self,others):
+    def set_location(self,others,tools):
         self.x = random.randint(self.bounds[0],self.bounds[2] - self.dimensions[2])
         self.y = random.randint(self.bounds[1],self.bounds[3] - self.dimensions[3])
         resetFlag = False
         for i in others:
-            if overlap(i,self):
-                resetFlag = True
+            for j in tools:
+                if overlap(i, self) or overlap(j, self):
+                    resetFlag = True
         if resetFlag:
-            self.set_location(others)
+            self.set_location(others, tools)
 
     def get_location(self):
         return (self.x,self.y)
@@ -52,44 +47,68 @@ class Boss(object):
             self.y = self.bounds[3]
 
 class Boss1(Boss):
-    def __init__(self,image,screen,others):
+    def __init__(self,image,screen,others,tools):
         self.image = image
         self.dimensions = image.get_rect()
         self.bounds = screen.get_rect()
-        self.set_location(others)
+        self.set_location(others,tools)
         self.direction_speed = 1
-        self.change_direction()
         self.click_needed = 1
 
 class Boss2(Boss):
-    def __init__(self,image,screen,others):
+    def __init__(self,image,screen,others,tools):
         self.image = image
         self.dimensions = image.get_rect()
         self.bounds = screen.get_rect()
-        self.set_location(others)
+        self.set_location(others,tools)
         self.direction_speed = 1
-        self.change_direction()
         self.click_needed = 2
 
 class Boss3(Boss):
-    def __init__(self,image,screen,others):
+    def __init__(self,image,screen,others,tools):
         self.image = image
         self.dimensions = image.get_rect()
         self.bounds = screen.get_rect()
-        self.set_location(others)
+        self.set_location(others,tools)
         self.direction_speed = 1
-        self.change_direction()
         self.click_needed = 3
 
 class Boss4(Boss):
-    def __init__(self,image,screen,others):
+    def __init__(self,image,screen,others,tools):
         self.image = image
         self.dimensions = image.get_rect()
         self.bounds = screen.get_rect()
-        self.set_location(others)
+        self.set_location(others,tools)
         self.direction_speed = 1
-        self.change_direction()
         self.click_needed = 4
+
+class Tool(object):
+    def get_location(self):
+        return (self.x,self.y)
+
+class Timer(Tool):
+    def __init__(self,image,screen):
+        self.image = image
+        self.dimensions = image.get_rect()
+        self.x = 25
+        self.y = 50
+
+class Exit(Tool):
+    def __init__(self,image,screen):
+        self.image = image
+        self.dimensions = image.get_rect()
+        self.x = 405
+        self.y = 25
+
+class Level_text(Tool):
+    def __init__(self,screen, level_counter):
+        self.font = pygame.font.Font(None, 30)
+        self.dimensions = [0, 0, 0, 0]
+        self.dimensions[2] = self.font.size("Level1")[0]
+        self.dimensions[3] = self.font.size("Level1")[1]
+        self.x = 32
+        self.y = 32
+        self.text = self.font.render('Level %d' % level_counter, True, (0, 0, 0), (255,255,255))
 
 def main():
     global play_again, level_counter
@@ -109,20 +128,35 @@ def main():
     start_banner_image = pygame.image.load('images/start_banner.png').convert_alpha()
     start_button_image = pygame.image.load('images/start_button.png').convert_alpha()
     level_complete_banner_image = pygame.image.load('images/level_complete_banner.png').convert_alpha()
+    start_again_banner_image = pygame.image.load('images/start_again_banner.png').convert_alpha()
     exit_image = pygame.image.load('images/exit.png').convert_alpha()
+    timer1_image = pygame.image.load('images/timer1.png').convert_alpha()
+    timer2_image = pygame.image.load('images/timer2.png').convert_alpha()
+    timer3_image = pygame.image.load('images/timer3.png').convert_alpha()
+    timer4_image = pygame.image.load('images/timer4.png').convert_alpha()
+    timer5_image = pygame.image.load('images/timer5.png').convert_alpha()
+    timer6_image = pygame.image.load('images/timer6.png').convert_alpha()
+    timer7_image = pygame.image.load('images/timer7.png').convert_alpha()
+    timer8_image = pygame.image.load('images/timer8.png').convert_alpha()
+    timer9_image = pygame.image.load('images/timer9.png').convert_alpha()
 
     quit_game = False
     start = False
     next_level = False
     game_music.play(-1)
-    font = pygame.font.Font(None, 30)
 
+    tools = []
+    exit = Exit(exit_image, screen)
+    tools.append(exit)
+    level_text = Level_text(screen, level_counter)
+    tools.append(level_text)
+    timer = Timer(timer1_image, screen)
+    tools.append(timer)
 
     while quit_game == False:
+
         while start == False and quit_game == False:
-
             tick = pygame.time.get_ticks()
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     stop_game = True
@@ -132,14 +166,14 @@ def main():
                         start = True
                     if event.pos[0] in range(405, 475) and event.pos[1] in range(25, 50):
                         quit_game = True
-
             screen.blit(background_image, (0,0))
-            screen.blit(exit_image, (405, 25))
+            exit = Exit(exit_image, screen)
+            screen.blit(exit.image, exit.get_location())
             screen.blit(start_banner_image, (0, 235))
             if tick/200 % 2 == 0:
                 screen.blit(start_button_image, (350, 260))
             else:
-                screen.blit(start_button_image, (350, 285))
+                screen.blit(start_button_image, (350, 270))
             pygame.display.update()
 
         characters = []
@@ -148,7 +182,7 @@ def main():
         boss1s = []
         boss1_image = pygame.image.load('images/boss1.png').convert_alpha()
         for i in xrange(num_boss1):
-            boss1 = Boss1(boss1_image,screen,characters)
+            boss1 = Boss1(boss1_image,screen,characters,tools)
             characters.append(boss1)
             boss1s.append(boss1)
 
@@ -157,7 +191,7 @@ def main():
         boss2s = []
         boss2_image = pygame.image.load('images/boss2.png').convert_alpha()
         for i in xrange(num_boss2):
-            boss2 = Boss2(boss2_image,screen,characters)
+            boss2 = Boss2(boss2_image,screen,characters,tools)
             characters.append(boss2)
             boss2s.append(boss2)
 
@@ -165,7 +199,7 @@ def main():
         boss3s = []
         boss3_image = pygame.image.load('images/boss3.png').convert_alpha()
         for i in xrange(num_boss3):
-            boss3 = Boss3(boss3_image,screen,characters)
+            boss3 = Boss3(boss3_image,screen,characters,tools)
             characters.append(boss3)
             boss3s.append(boss3)
 
@@ -173,14 +207,15 @@ def main():
         boss4s = []
         boss4_image = pygame.image.load('images/boss4.png').convert_alpha()
         for i in xrange(num_boss4):
-            boss4 = Boss4(boss4_image,screen,characters)
+            boss4 = Boss4(boss4_image,screen,characters,tools)
             characters.append(boss4)
             boss4s.append(boss4)
 
+        leveltick_start = pygame.time.get_ticks()
         while start == True and quit_game == False:
             next_level = False
             tick = pygame.time.get_ticks()
-
+            leveltick = pygame.time.get_ticks()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     stop_game = True
@@ -220,9 +255,42 @@ def main():
                         quit_game = True
 
             screen.blit(background_image, (0,0))
-            screen.blit(exit_image, (405, 25))
-            level_text = font.render('Level %d' % level_counter, True, (0, 0, 0), (255,255,255))
-            screen.blit(level_text,(32,32))
+            screen.blit(exit.image, exit.get_location())
+            level_text = Level_text(screen, level_counter)
+            screen.blit(level_text.text,level_text.get_location())
+
+            if leveltick > leveltick_start and leveltick < leveltick_start + 1875:
+                timer1 = Timer(timer1_image, screen)
+                screen.blit(timer1_image, timer1.get_location())
+            elif leveltick >= leveltick_start + 1875 and leveltick < leveltick_start + 3750:
+                timer2 = Timer(timer2_image, screen)
+                screen.blit(timer2_image, timer2.get_location())
+            elif leveltick >= leveltick_start + 3750 and leveltick < leveltick_start + 5625:
+                timer3 = Timer(timer3_image, screen)
+                screen.blit(timer3_image, timer3.get_location())
+            elif leveltick >= leveltick_start + 5625 and leveltick < leveltick_start + 7500:
+                timer4 = Timer(timer4_image, screen)
+                screen.blit(timer4_image, timer4.get_location())
+            elif leveltick >= leveltick_start + 7500 and leveltick < leveltick_start + 9375:
+                timer5 = Timer(timer5_image, screen)
+                screen.blit(timer5_image, timer5.get_location())
+            elif leveltick >= leveltick_start + 9375 and leveltick < leveltick_start + 11250:
+                timer6 = Timer(timer6_image, screen)
+                screen.blit(timer6_image, timer6.get_location())
+            elif leveltick >= leveltick_start + 11250 and leveltick < leveltick_start + 13125:
+                timer7 = Timer(timer7_image, screen)
+                screen.blit(timer7_image, timer7.get_location())
+            elif leveltick >= leveltick_start + 13125 and leveltick < leveltick_start + 15000:
+                timer8 = Timer(timer8_image, screen)
+                screen.blit(timer8_image, timer8.get_location())
+            elif leveltick >= leveltick_start + 15000:
+                timer9 = Timer(timer9_image, screen)
+                screen.blit(timer9_image, timer9.get_location())
+                sound = pygame.mixer.Sound('sounds/lose.wav')
+                sound.play()
+                end_condition = 'lose'
+                level_counter = 1
+                break
             for i in characters:
                 screen.blit(i.image, i.get_location())
             pygame.display.update()
@@ -233,18 +301,10 @@ def main():
                 end_condition = 'win'
                 level_counter += 1
                 break
-            #else:
-            #    sound = pygame.mixer.Sound('sounds/lose.wav')
-            #    sound.play()
-            #    end_condition = 'lose'
-            #    level_counter = 1
-            #    break
 
             game_music.stop()
 
         while start == True and end_condition == 'win' and quit_game == False:
-            print "enter 3rd loop"
-            print "next level: ", next_level
             tick = pygame.time.get_ticks()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -259,14 +319,38 @@ def main():
                 break
 
             screen.blit(background_image, (0,0))
-            screen.blit(exit_image, (405, 25))
-            if end_condition == 'win':
-                screen.blit(level_complete_banner_image, (0, 235))
-                if tick/200 % 2 == 0:
-                    screen.blit(start_button_image, (350, 300))
-                else:
-                    screen.blit(start_button_image, (350, 285))
-                pygame.display.update()
+            screen.blit(exit.image, exit.get_location())
+            screen.blit(level_complete_banner_image, (0, 235))
+            if tick/200 % 2 == 0:
+                screen.blit(start_button_image, (350, 300))
+            else:
+                screen.blit(start_button_image, (350, 290))
+            pygame.display.update()
+
+        while start == True and end_condition == 'lose' and quit_game == False:
+            tick = pygame.time.get_ticks()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit_game = True
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.pos[0] in range(350, 450) and event.pos[1] in range(285, 400):
+                        next_level = True
+                        print next_level
+                        print level_counter
+                        break
+                    if event.pos[0] in range(405, 475) and event.pos[1] in range(25, 50):
+                        quit_game = True
+            if next_level == True:
+                break
+
+            screen.blit(background_image, (0,0))
+            screen.blit(exit.image, exit.get_location())
+            screen.blit(start_again_banner_image, (0, 235))
+            if tick/200 % 2 == 0:
+                screen.blit(start_button_image, (350, 260))
+            else:
+                screen.blit(start_button_image, (350, 270))
+            pygame.display.update()
 
 """
         if tick > 2000:
